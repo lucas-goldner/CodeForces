b5bca04f94e2d3865918f25274084dc36dbc52a1
lucas-goldner <lucas.goldner@googlemail.com>
2023-01-30 01:52:34 +0100

    feat: Implementation of theatresquare problem

--- problemsets/1-10/1A_theatresquare.cpp
+++ problemsets/1-10/1A_theatresquare.cpp
@@ -1,7 +1,18 @@
+// Problem: https://codeforces.com/problemset/problem/1/A
+
 #include <iostream>
+#include <cmath>
+using namespace std;
+
+// Expected results
+// 6 6 4 -> 4
+// 1 1 1 -> 1
 
 int main()
 {
-    std::cout << "Hello World\n";
+    double n, m, a, numberOfFlagstones;
+    cin >> n >> m >> a;
+    numberOfFlagstones = ceil(m / a) * ceil(n / a);
+    cout << numberOfFlagstones;
     return 0;
 }
