08abf874abce584618b4751c83d51c1d69f3a941
lucas-goldner <lucas.goldner@googlemail.com>
2023-01-30 02:06:06 +0100

    fix: Look out for long numbers and set precision

--- problemsets/1-10/1A_theatresquare.cpp
+++ problemsets/1-10/1A_theatresquare.cpp
@@ -2,17 +2,20 @@
 
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <iomanip>
 using namespace std;
 
 // Expected results
 // 6 6 4 -> 4
 // 1 1 1 -> 1
+// 1000000000 1000000000 1 -> 1000000000000000000
 
 int main()
 {
     double n, m, a, numberOfFlagstones;
     cin >> n >> m >> a;
     numberOfFlagstones = ceil(m / a) * ceil(n / a);
-    cout << numberOfFlagstones;
+    cout << fixed << setprecision(0) << numberOfFlagstones;
     return 0;
 }
