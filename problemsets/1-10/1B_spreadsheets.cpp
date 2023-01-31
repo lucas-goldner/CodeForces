d2ed41919806631fc5ebde26f37e6d30e3ad3ab2
lucas-goldner <lucas.goldner@googlemail.com>
2023-01-31 22:28:02 +0100

    refactor: Rewrite logic of getLettersForNumber function

--- problemsets/1-10/1B_spreadsheets.cpp
+++ problemsets/1-10/1B_spreadsheets.cpp
@@ -19,6 +19,8 @@ using namespace std;
 //
 // 1
 // R621C406 ->  OP621
+// 1
+// R875C898 ->  AHN875
 
 // Notes:
 // RXCY system -> X and Y value e.g: R23C55
@@ -38,17 +40,23 @@ string getLettersForNumber(string number)
     string letters = "";
     int current_number = stoi(number);
 
-    int quotient = current_number / 26;
-    if (quotient != 0)
+    while (current_number > 0)
     {
-        char letterOne = char(quotient + 64);
-        letters.push_back(letterOne);
+        int remainder = current_number % 26;
+        if (remainder == 0)
+        {
+            letters.push_back('Z');
+            current_number = (current_number / 26) - 1;
+        }
+        else
+        {
+            char letter = char(remainder + 64);
+            letters.push_back(letter);
+            current_number = current_number / 26;
+        }
     }
 
-    int remainder = current_number % 26;
-    char letterTwo = char(remainder + 64);
-    letters.push_back(letterTwo);
-
+    reverse(letters.begin(), letters.end());
     return letters;
 }
 
@@ -95,7 +103,7 @@ string convert_from_one_system_to_other(string input)
         regex pattern("[A-Za-z]+([0-9]+)");
         smatch result;
         regex_search(input, result, pattern);
-        int num = stoi(result[1].str());
+        int num = std::stoi(result[1].str());
         int num_index = input.find(to_string(num));
         string y_substring = input.substr(0, num_index);
         return convert_to_rcxy_system(y_substring, num);
