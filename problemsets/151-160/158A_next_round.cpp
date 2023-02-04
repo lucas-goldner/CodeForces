deb1c4a0146a09415a9960450df20f40023aef81
lucas-goldner <lucas.goldner@googlemail.com>
2023-02-04 21:45:36 +0100

    feat: Implement next round problem

---
+++ problemsets/151-160/158A_next_round.cpp
@@ -0,0 +1,43 @@
+// Problem: https://codeforces.com/problemset/problem/158/A
+#include <iostream>
+using namespace std;
+
+// Expected results:
+// 8 5
+// 10 9 8 7 7 7 5 5 -> 6
+//
+// 4 2
+// 0 0 0 0 -> 0
+
+// Notes:
+// Contestant who earns a score equal to or greater than the k-th place finisher's score will advance to the next round
+
+int count_player_that_will_pass(int n, int k, int player_scores[])
+{
+    int players_that_will_pass = 0;
+    int score_to_beat = player_scores[k];
+    for (int i = 0; i <= n - 1; i++)
+    {
+        if (player_scores[i] >= score_to_beat && player_scores[i] > 0)
+        {
+            players_that_will_pass++;
+        }
+    }
+
+    return players_that_will_pass;
+}
+
+int main()
+{
+    int number_of_participants, position_of_player;
+    scanf("%i %i", &number_of_participants, &position_of_player);
+    int player_scores[number_of_participants];
+    for (int i = 0; i <= number_of_participants - 1; i++)
+    {
+        cin >> player_scores[i];
+    }
+
+    cout << count_player_that_will_pass(number_of_participants, position_of_player, player_scores);
+
+    return 0;
+}
