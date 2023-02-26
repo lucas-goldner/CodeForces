a2392fdc791d72b5afd88605f56593d92a8c2feb
lucas-goldner <lucas.goldner@googlemail.com>
2023-02-26 19:31:45 +0100

    refactor: Adjust implementation of 116A tram

--- problemsets/111-120/116A_tram.cpp
+++ problemsets/111-120/116A_tram.cpp
@@ -11,40 +11,24 @@ using namespace std;
 // 4 2
 // 4 0
 
-struct stop
+int main()
 {
-    int passengers_entering = 0, passengers_leaving = 0;
-};
-
-int calculate_maximum_capicity(stop stops[], int number_of_stops)
-{
-    int max_capicity = 0;
-    int current_passengers = 0;
+    int number_of_stops(0), max_capicity(0), current_passengers(0);
+    cin >> number_of_stops;
     for (int i = 0; i <= number_of_stops - 1; i++)
     {
-        current_passengers -= stops[i].passengers_leaving;
-        current_passengers += stops[i].passengers_entering;
-        if (max_capicity <= current_passengers)
+        int entering(0), leaving(0);
+        scanf("%i %i", &leaving, &entering);
+
+        current_passengers -= leaving;
+        current_passengers += entering;
+        if (current_passengers > max_capicity)
         {
             max_capicity = current_passengers;
         }
     }
-    return max_capicity;
-}
 
-int main()
-{
-    int number_of_stops = 0;
-    stop stops[number_of_stops];
-    cin >> number_of_stops;
-    for (int i = 0; i <= number_of_stops - 1; i++)
-    {
-        scanf("%i %i", &stops[i].passengers_leaving, &stops[i].passengers_entering);
-    }
-
-    int capicity = calculate_maximum_capicity(stops, number_of_stops);
-
-    cout << capicity << endl;
+    cout << max_capicity << endl;
 
     return 0;
 }
